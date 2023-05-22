6b6ef3b56791efbc5cb576fb2cc2d81392f09209
Salikh Valitov <salikhcomhd@gmail.com>
2023-05-22 17:32:16 +0500

    добавлены роуты и middleware

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { default: axios } = require('axios');
+require('dotenv').config();
+
+const app = express();
+app.use(bodyParser.json());
+
+const _routes = [];
+const _clients = [];
+const _cargos = [];
+
+const getRoutesMiddleware = (req, res, next) => {
+  const points = req.body.routes.map(route => route.split('-'));
+  const routes = points.map(route => route.slice(1, -1));
+  req.routes = routes.map(route => route.map(point => +point));
+  req.clientsId = [...req.routes.flat()];
+  _routes = req.routes;
+  //console.log('POST requests with:', req.routes);
+  //console.log('POST requests with:', req.clientsId);
+  next(); 
+};
+
+const readDBMiddleware = async (req, res, next) => {
+  const response = await axios.get('http://localhost:3000/api/clients');
+  req.clients = await response.data;
+  //console.log('readDB:', clients);
+  next();
+};
+
+const getGargosMiddleware = async (req, res, next) => {
+    const clients = req.clients;
+    const response = await axios.get('http://localhost:3000/api/cargos');
+    req.cargos = await response.data;
+    _cargos = req.cargos;
+    next();
+};
+
+const setPartMiddleware = (req, res, next) => {
+    for (let id of req.clientsId) {
+      _clients.push(_cargos.filter(cargo => cargo.pointId === id))
+    }
+    next();
+};
+
+const getMiddleware = (req, res, next) => {
+  req.volume = [{ id: 1, volume: '0.55'}, { id: 2, volume: '0.65'}, { id: 3, volume: '0.75'}]; //передать из мидла в обработчик
+  next(); 
+};
+
+app.get('/api/pack', getMiddleware, (req, res) => {
+  const volume = req.volume;
+  res.send({ success: 1, volume: volume });
+});
+
+const postMiddleware = [getRoutesMiddleware, readDBMiddleware, getGargosMiddleware, setPartMiddleware];
+
+app.post('/api/routes', postMiddleware, (req, res) => {
+  const routes = req.body.routes;
+  
+  if (!routes) {
+    return res.status(400).json({ error: 'No routes provided.' });
+  }
+
+  if (!Array.isArray(routes)) {
+    return res.status(400).json({ error: 'Routes should be an array.' });
+  }
+
+  return res.status(200).json({ success: true, message: 'Routes received and processed successfully.' });
+});
+
+const PORT = process.env.PORT || 5007;
+app.listen(PORT, () => console.log(`🚀 @ http://localhost:${PORT}`));
---
+++ unuse.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import { json as jsonParser } from 'body-parser';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(jsonParser());
+
+// Middleware to process routes from the request
+const processRoutesMiddleware = (req, res, next) => {
+  const { routes } = req.body;
+  
+  if (!routes) {
+    return res.status(400).json({ error: 'No routes provided.' });
+  }
+
+  if (!Array.isArray(routes)) {
+    return res.status(400).json({ error: 'Routes should be an array.' });
+  }
+
+  req.processedRoutes = routes.map(route => route.split('-').slice(1, -1));
+  console.log('POST requests with:', req.processedRoutes);
+
+  next(); 
+};
+
+// Middleware to add volume to the request
+const addVolumeMiddleware = (req, res, next) => {
+  req.volume = [
+    { id: 1, volume: '0.55'}, 
+    { id: 2, volume: '0.65'}, 
+    { id: 3, volume: '0.75'}
+  ];
+  
+  next(); 
+};
+
+app.get('/api/pack', addVolumeMiddleware, (req, res) => {
+  res.send({ success: 1, volume: req.volume });
+});
+
+app.post('/api/routes', processRoutesMiddleware, (req, res) => {
+  return res.status(200).json({ success: true, message: 'Routes received and processed successfully.' });
+});
+
+const PORT = process.env.PORT || 5007;
+app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
