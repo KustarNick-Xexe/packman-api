8cc087cccbfcf8d29700397d95692cbfefc7522a
Salikh Valitov <salikhcomhd@gmail.com>
2023-05-26 11:08:19 +0500

    новые исправления

--- app.js
+++ app.js
@@ -21,77 +21,54 @@ const getRoutesMiddleware = (req, res, next) => {
 };
 
 const getGargosMiddleware = async (req, res, next) => {
-  const response = await axios.get('http://localhost:3000/api/cargos');
-  req.cargos = await response.data;
+  const response = await axios.get('http://localhost:3007/api/cargos');
+  req.cargos = response.data;
   _cargos = req.cargos;
   next();
 };
 
-const clientCargos = [];
-const boxesSet = [];
+let clientCargos = [];
 let binWidth = 0;
 let binHeight = 0;
 let binDepth = 0;
 
 const getMiddleware = async (req, res, next) => {
-  /* const clients = Array.from(new Set(_routes.flat()));
+  const clients = Array.from(new Set(_routes.flat()));
+  console.log(clients);
   for (const id of clients) {
-    const cargos = _cargos.filter(cargo => cargo.idClient === id);
-    clientCargos.push({ id: id, cargos: cargos });
+    const cargos = _cargos.map(cargo => { 
+      if(cargo.idClient === id) {
+        return [ cargo.id, cargo.width, cargo.length, cargo.height ];
+      }
+    });
+    clientCargos.push({ id: id, cargos: cargos.filter(cargo => cargo !== undefined) });
   };
-  const response = await axios.get('http://localhost:3000/api/vehicles');
+
+  console.log(clientCargos);
+
+  const response = await axios.get('http://localhost:3007/api/vehicles');
   const vehicles = response.data;
   const vehicle = vehicles[0];
   binWidth = vehicle.width;
-  binHeight = vehicle.height;
-  binDepth = vehicle.depth; */
+  binHeight = vehicle.length;
+  binDepth = vehicle.height;
   next();
 };
 
 app.get('/api/pack', getMiddleware, (req, res) => {
-  /* _routes.forEach(route => {
-    const boxes = [];
-    route.forEach(idClient => {
-      clientCargos.forEach(cargosWithId => {
-        if (cargosWithId.id === idClient) {
-          boxes.push(cargosWithId.cargos);
-        }
-      })
-    });
-    boxesSet.push(boxes);
-  })
-
-  console.log(boxesSet);
-
-  for(let i = 0; i < _routes.length; i++) {
-    const bin =  new Bin(binWidth, binHeight, binDepth);
-
-    let success = false;
-    boxesSet.forEach(set => {
-      set.forEach(boxes => {
-        const boxesCount = boxes.length;
-        const resultCount = bin.pack(boxes).length;
-
-        if(boxesCount === resultCount) { success = true; }
-        else { success = false; }
-      });
-    });
-  } */
-
+  //console.log(binWidth, binHeight, binDepth);
   const answer = Math.random() > 0.35 ? 1 : 0;
+  _cargos = [];
+  _routes = [];
+  clientCargos = [];
   return res.status(200).json({ success: answer, volume: 50 });
 });
 
 const postMiddleware = [getRoutesMiddleware, getGargosMiddleware];
 
 app.post('/api/routes', postMiddleware, (req, res) => {
-  const routes = req.body.routes;
-  if (!routes) {
-    return res.status(400).json({ error: 'No routes provided.' });
-  }
-  if (!Array.isArray(routes)) {
-    return res.status(400).json({ error: 'Routes should be an array.' });
-  }
+  //console.log(_cargos);
+  //console.log(_routes);
   return res.status(200).json({ success: 1 });
 });
 
--- classes/indexTrue.js
+++ classes/indexTrue.js
@@ -1,12 +1,12 @@
 class Box {
     constructor(w, h, d, fragile = false, orientation = 0) {
-      this.x = 0; this.y = 0; this.z = 0;
+        this.x = 0; this.y = 0; this.z = 0;
         this.w = w; this.h = h; this.d = d;
         this.fragile = fragile;
         this.orientation = orientation;
     }
     get coordinates() {
-      return [this.x, this.y, this.z];
+        return [this.x, this.y, this.z];
     }
     get dimensions() {
         switch (this.orientation) {
@@ -40,39 +40,39 @@ class Bin {
 
     pack(boxes) {
         let placements = [];
-      boxes = boxes.sort((a, b) => b.volume - a.volume);
-        for(let box of boxes) {
+        boxes = boxes.sort((a, b) => b.volume - a.volume);
+        for (let box of boxes) {
             let [boxWidth, boxHeight, boxDepth] = box.dimensions;
             outer:
-            for(let x = 0; x <= this.binWidth - boxWidth; x++) {
-                for(let y = 0; y <= this.binHeight - boxHeight; y++) {
-                    for(let z = 0; z <= this.binDepth - boxDepth; z++) {
+            for (let x = 0; x <= this.binWidth - boxWidth; x++) {
+                for (let y = 0; y <= this.binHeight - boxHeight; y++) {
+                    for (let z = 0; z <= this.binDepth - boxDepth; z++) {
                         let isFree = true;
-                        for(let i = 0; i < boxWidth; i++) {
-                            for(let j = 0; j < boxHeight; j++) {
-                                for(let k = 0; k < boxDepth; k++) {
-                                    if(this._bin[x+i][y+j][z+k] !== 0) {
+                        for (let i = 0; i < boxWidth; i++) {
+                            for (let j = 0; j < boxHeight; j++) {
+                                for (let k = 0; k < boxDepth; k++) {
+                                    if (this._bin[x + i][y + j][z + k] !== 0) {
                                         isFree = false;
                                         break;
                                     }
                                 }
-                                if(!isFree) break;
+                                if (!isFree) break;
                             }
-                            if(!isFree) break;
+                            if (!isFree) break;
                         }
-                        if(isFree) {
-                            for(let i = 0; i < boxWidth; i++) {
-                                for(let j = 0; j < boxHeight; j++) {
-                                    for(let k = 0; k < boxDepth; k++) {
-                                        this._bin[x+i][y+j][z+k] = box.fragile ? 2 : 1;
+                        if (isFree) {
+                            for (let i = 0; i < boxWidth; i++) {
+                                for (let j = 0; j < boxHeight; j++) {
+                                    for (let k = 0; k < boxDepth; k++) {
+                                        this._bin[x + i][y + j][z + k] = box.fragile ? 2 : 1;
                                     }
                                 }
                             }
-                            if(box.fragile) {
-                                for(let i = 0; i < boxWidth; i++) {
-                                    for(let j = 0; j < boxHeight; j++) {
-                                        for(let k = z + boxDepth; k < this.binDepth; k++) {
-                                            this._bin[x+i][y+j][k] = 2;
+                            if (box.fragile) {
+                                for (let i = 0; i < boxWidth; i++) {
+                                    for (let j = 0; j < boxHeight; j++) {
+                                        for (let k = z + boxDepth; k < this.binDepth; k++) {
+                                            this._bin[x + i][y + j][k] = 2;
                                         }
                                     }
                                 }
@@ -80,7 +80,7 @@ class Bin {
                             box.x = x;
                             box.y = y;
                             box.z = z;
-                            placements.push({box: box, x: x, y: y, z: z, orientation: box.orientation});
+                            placements.push({ box: box, x: x, y: y, z: z, orientation: box.orientation });
                             break outer;
                         }
                     }
@@ -91,18 +91,29 @@ class Bin {
     }
 
     save(placements) {
-        this._boxes = [  ...this._boxes, ...placements ];
-       for (let packedBox of this._boxes) {
-          const x = packedBox[0];
-          const y = packedBox[1];
-          const z = packedBox[2];
-          const w = packedBox[3];
-          const h = packedBox[4];
-          const d= packedBox[5];
+        this._boxes = [...this._boxes, ...placements];
+    }
+
+    reset() {
+        this._bin = new Array(this.binWidth);
+        for (let i = 0; i < this.binWidth; i++) {
+            this._bin[i] = new Array(this.binHeight);
+            for (let j = 0; j < this.binHeight; j++) {
+                this._bin[i][j] = new Array(this.binDepth).fill(0);
+            }
+        }
+
+        for (let packedBox of this._boxes) {
+            const x = packedBox[0];
+            const y = packedBox[1];
+            const z = packedBox[2];
+            const w = packedBox[3];
+            const h = packedBox[4];
+            const d = packedBox[5];
             for (let i = x; i <= w; i++) {
                 for (let j = y; j <= h; j++) {
                     for (let k = z; k <= d; k++) {
-                        this._bin[x + i][y + j][z + k] = box.fragile ? 2 : 1;
+                        this._bin[x + i][y + j][z + k] = packedBox.fragile ? 2 : 1;
                     }
                 }
             }
@@ -110,12 +121,43 @@ class Bin {
     }
 }
 
+function isPackable(boxes, orientations, bin) {
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].orientation = orientations[i];
+    }
+    const packingResult = bin.pack(boxes);
+    bin.reset();
+    return packingResult.length === boxes.length;
+}
+
+function setOrientation(boxes, orientations) {
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].orientation = orientations[i];
+    }
+    
+    return boxes;
+}
+
 const boxes1 = [
+    new Box(1, 1, 2),
+    new Box(1, 2, 1, true),
+    new Box(2, 1, 1),
+    new Box(1, 1, 2),
+    new Box(1, 2, 1, true),
+    new Box(2, 1, 1),
     new Box(1, 1, 2),
     new Box(1, 2, 1, true),
     new Box(2, 1, 1),
 ];
 const boxes2 = [
+    new Box(1, 1, 2, true),
+    new Box(1, 2, 1, true),
+    new Box(2, 1, 1),
+    new Box(1, 1, 2),
+    new Box(1, 1, 2, true),
+    new Box(1, 2, 1, true),
+    new Box(2, 1, 1),
+    new Box(1, 1, 2),
     new Box(1, 1, 2, true),
     new Box(1, 2, 1, true),
     new Box(2, 1, 1),
@@ -127,13 +169,51 @@ const boxes3 = [
     new Box(1, 2, 1),
     new Box(2, 1, 1),
     new Box(1, 1, 2, true),
+    new Box(1, 1, 2),
+    new Box(1, 2, 1),
+    new Box(2, 1, 1),
+    new Box(1, 1, 2, true),
+    new Box(1, 1, 2),
+    new Box(1, 2, 1),
+    new Box(2, 1, 1),
+    new Box(1, 1, 2, true),
 ];
 
-const bin = new Bin(10, 10, 10);
-bin.pack(boxes1);
-bin.save(boxes1);
-bin.pack(boxes2);
-bin.save(boxes2);
-bin.pack(boxes3);
-bin.save(boxes3);
-console.log(bin._boxes.map(box => [box.x, box.y, box.z, box.w, box.h, box.d, box.fragile]))
+let bins = [];
+for (let i = 0; i < 5; i++) {
+    bins.push(new Bin(10, 10, 10));
+}
+
+const boxes = [boxes1, boxes2, boxes3];
+
+bins = bins.map(bin => {
+    const variants = [];
+    boxes.forEach(_boxes => {
+        let orientations = [];
+        for (let i = 0; i < 5; i++) {
+            do {
+                orientations = [];
+                for (let j = 0; j < _boxes.length; j++) {
+                    const orientation = Math.floor(Math.random() * 6);
+                    orientations.push(orientation);
+                }
+            } while (!isPackable(_boxes, orientations, bin));
+        
+        }
+        _boxes = setOrientation(_boxes, orientations);
+        variants.push(orientations);
+        bin.pack(_boxes);
+        bin.reset();
+        bin.save(_boxes);
+    })
+
+    return bin;
+});
+
+bins.forEach(bin => {
+    console.log(bin._boxes.map(box => [box.x, box.y, box.z, box.w, box.h, box.d, box.fragile]));
+})
+
+/* console.log(bins.map(bin => {
+    bin._boxes.map(box => [box.x, box.y, box.z, box.w, box.h, box.d, box.fragile])
+})) */
